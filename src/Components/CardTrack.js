a170615951cfeca7d39e902a54b5e0cbd18c9b5f
Soumya1998-18 <107382070+Soumya1998-18@users.noreply.github.com>
2022-09-12 00:00:22 +0530

    Update CardTrack.js

--- src/Components/CardTrack.js
+++ src/Components/CardTrack.js
@@ -5,7 +5,7 @@ const Track = props => {
   const { track } = props;
 
   return (
-    <div className="col-md-6">
+    <div className="col-md-4">
       <div className="card mb-4 shadow-sm">
         <div className="card-body">
           <h5>{track.artist_name}</h5>
